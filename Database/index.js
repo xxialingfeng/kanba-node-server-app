694ca591e796495073a1845cd21c195d7c08e446
xialingfeng <lingfengxia.jake@gmail.com>
2023-11-09 14:49:23 -0500

    modify assert

--- Database/index.js
+++ Database/index.js
@@ -2,8 +2,8 @@ import courses from "./courses.json" assert { type: "json" };
 import modules from "./modules.json" assert { type: "json" };
 import assignments from "./assignments.json" assert { type: "json" };
 import users from "./users.json" assert { type: "json" };
-import enrollments from "./enrollments.json" assert { type: "json" };
 import grades from "./grades.json" assert { type: "json" };
+import enrollments from "./enrollments.json" assert { type: "json" };
 
 const data = {
   courses,
