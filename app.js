298f681590ca3d04775d2a71d27d01ca39fde289
xialingfeng <lingfengxia.jake@gmail.com>
2023-11-30 15:05:57 -0500

    a6

--- app.js
+++ app.js
@@ -2,7 +2,10 @@ import "dotenv/config";
 import express from 'express'
 import Hello from "./hello.js"
 import Lab5 from "./Lab5.js"
+import UserRoutes from "./users/routes.js";
 import cors from "cors";
+import mongoose from "mongoose";
+mongoose.connect("mongodb://127.0.0.1:27017/Kanbas");
 import CourseRoutes from "./courses/routes.js";
 import ModuleRoutes from "./modules/routes.js";
 import AssignmentRoutes from './assignments/routes.js';
@@ -11,25 +14,41 @@ const app = express();
 app.set("trust proxy", 1);
 app.use(cors({
   credentials: true,
-  origin: "http://localhost:3000",
+  origin: "https://a6--peppy-parfait-96bac1.netlify.app",
 }
 ));
 app.use(
-    session({
-      secret: "any string",
-      resave: false,
-      proxy: true,
-      saveUninitialized: false,
-      cookie: {
-        sameSite: "none",
-        secure: true,
-      },
-    })
-   );
+  session({
+    secret: "any string",
+    resave: false,
+    proxy: true,
+    saveUninitialized: false,
+    cookie: {
+      sameSite: "none",
+      secure: true,
+    },
+  })
+);
+const sessionOptions = {
+  secret: "any string",
+  resave: false,
+  saveUninitialized: false,
+};
+if (process.env.NODE_ENV !== "development") {
+  sessionOptions.proxy = true;
+  sessionOptions.cookie = {
+    sameSite: "none",
+    secure: true,
+  };
+}
+app.use(session(sessionOptions));
+
+  
 app.use(express.json());
+UserRoutes(app);
 ModuleRoutes(app);
 CourseRoutes(app);
 AssignmentRoutes(app);
 Hello(app)
 Lab5(app);
-app.listen(process.env.PORT || 4000);
+app.listen(4000);
